6526bd1dff9ff987d48bce22c2e89577c774c30c
Andree-Zzz <mad08061@gmail.com>
2022-02-24 21:24:41 -0500

    Iniciando repositorio

---
+++ main.py
@@ -0,0 +1,28 @@
+from ast import parse
+from datetime import datetime
+from operator import concat
+from xml.dom.expatbuilder import parseString
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return render_template('index.html')
+
+@app.get("/contacto")
+def contacto():
+    return render_template('contactos/index.html')
+
+@app.get("/contacto/<int:contactoId>/editar")
+def editarContacto(contactoId):
+    year = datetime.now().year
+    # edad = year - int(contactoId)
+    edad = year - contactoId
+    return render_template(
+        'contactos/editar.html',
+        contactoId = contactoId,
+        edad = edad,
+    )
+
+app.run(debug=True)
