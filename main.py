f21594f8298552e72d42a9d78dce216742d84621
Andree-Zzz <mad08061@gmail.com>
2022-03-03 21:53:00 -0500

    Base de datos y demas

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'productos',
+    port = '3306'
+)
+db.autocommit = True # Desactivar el cache
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return render_template('index.html')
+    # Apertura del cursor, dictionary Treu formato leible
+    cursor = db.cursor(dictionary = True)
+
+    cursor.execute('SELECT * FROM productos')
+    # Obtener todo lo de la consulta
+    productos = cursor.fetchall()
+    # print(productos[1]['nombre'])
+    # Obtiene solo el primer valor de la consulta
+    # productosOne = cursor.fetchone()
+
+    # Cierre del cursor
+    cursor.close()
+
+    return render_template('index.html', productos = productos)
 
 @app.get("/contacto")
 def contacto():
     return render_template('contactos/index.html')
+
+@app.get("/crear")
+def crearProducto():
+    return render_template("crear.html")
+
+@app.post("/crear")
+def crearProductoPost():
+    nombre = request.form.get('nombre')
+    precio = request.form.get('precio')
+
+    cursor = db.cursor()
+
+    # ( nombre,precio) => tupla, lista no cambiante
+    cursor.execute("INSERT INTO productos(nombre,precio) VALUES(%s,%s)",(
+        nombre,
+        precio,
+    ))
+
+    cursor.close()
+    
+    return redirect(url_for('index'))
 
 @app.get("/contacto/<int:contactoId>/editar")
 def editarContacto(contactoId):
